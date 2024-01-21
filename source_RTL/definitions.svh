d28e3ffd6fde47af4ceedd0b44714ac462723ffb
TR174 <58868716+TR174@users.noreply.github.com>
2024-01-20 20:46:41 -0500

    Delete source_RTL directory

--- source_RTL/definitions.svh
+++
@@ -1,8 +0,0 @@
-/*
-PARAMETERS FOR PUF CONTROL MODULE
-*/
-parameter data_width = 32;
-parameter addr_width = 32;
-parameter puf_sig_length = 256;
-parameter valid_byte_offset = 16;
-parameter parity_bits_width = 48;
